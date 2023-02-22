c705edb6969ac668467079206b9f97ef854d9114
PiHyoJeong <shson4007@gmail.com>
2023-02-22 23:57:08 +0900

    Chore: BookHistory 기본세팅 추가

---
+++ src/main/java/com/company/library/dto/BookHistoryDto.java
@@ -0,0 +1,16 @@
+package com.company.library.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+public class BookHistoryDto {
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class RequestBookDto{
+
+    }
+}
